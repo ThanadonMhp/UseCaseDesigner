34ba501d82defda9271cfc6436b86fc5f3b8bd87
Thanadon Mhp <thanadon.mhp@gmail.com>
2024-04-05 16:37:12 +0700

    Fix application resolution and minor details.

--- src/main/java/ku/cs/usecasedesigner/HelloApplication.java
+++ src/main/java/ku/cs/usecasedesigner/HelloApplication.java
@@ -11,7 +11,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("homepage.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1550, 800);
+        Scene scene = new Scene(fxmlLoader.load(), 1280, 720);
         stage.setTitle("UseCaseGenerate");
         stage.setScene(scene);
         stage.show();
--- src/main/java/ku/cs/usecasedesigner/controller/HomepageController.java
+++ src/main/java/ku/cs/usecasedesigner/controller/HomepageController.java
@@ -1,14 +1,23 @@
 package ku.cs.usecasedesigner.controller;
+
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.*;
+
 public class HomepageController {
     @FXML
     private ImageView ovalImageView;
 
     @FXML
-    void handleDragDetected(MouseEvent event) {
+    void initialize() {
+        ovalImageView.setOnDragDetected(this::handleDragDetected);
+        ovalImageView.setOnDragOver(this::handleDragOver);
+        ovalImageView.setOnDragDropped(this::handleDragDropped);
+    }
+
+    @FXML
+    private void handleDragDetected(MouseEvent event) {
         Dragboard dragboard = ovalImageView.startDragAndDrop(TransferMode.COPY);
 
         ClipboardContent content = new ClipboardContent();
@@ -20,23 +29,28 @@ public class HomepageController {
     }
 
     @FXML
-    void handleDragOver(DragEvent event) {
-        event.getDragboard().hasImage();
-        event.acceptTransferModes(TransferMode.COPY);
-
+    private void handleDragOver(DragEvent event) {
+        if (event.getGestureSource() != ovalImageView &&
+                event.getDragboard().hasImage()) {
+            event.acceptTransferModes(TransferMode.COPY);
+        }
 
         event.consume();
     }
 
     @FXML
-    void handleDragDropped(DragEvent event) {
+    private void handleDragDropped(DragEvent event) {
         boolean success = false;
 
         if (event.getDragboard().hasImage()) {
             ImageView imageView = new ImageView(event.getDragboard().getImage());
-            imageView.setFitWidth(100); // ปรับขนาดของรูปภาพตามที่คุณต้องการ
-            imageView.setFitHeight(100); // ปรับขนาดของรูปภาพตามที่คุณต้องการ
-//            imageView.getChildren().add(imageView);
+            imageView.setFitWidth(100);
+            imageView.setFitHeight(100);
+
+            // Add the image to the container or pane where you want to drop it
+            // For example, if you have a Pane named "containerPane", you can do:
+            // containerPane.getChildren().add(imageView);
+
             success = true;
         }
 
