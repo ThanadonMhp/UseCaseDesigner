cd8602ca592cafc81aaf0eedc3ce77c705c6d5ea
Thanadon Mhp <thanadon.mhp@gmail.com>
2024-04-12 21:02:59 +0700

    Fix Route Configure

--- src/main/java/ku/cs/usecasedesigner/ProjectApplication.java
+++ src/main/java/ku/cs/usecasedesigner/ProjectApplication.java
@@ -9,15 +9,15 @@ import java.io.IOException;
 public class ProjectApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXRouter.bind(this, stage, "UseCaseDesigner | Homepage", 1280, 720);
+        FXRouter.bind(this, stage, "UseCaseDesigner | RootLayout", 1280, 720);
         configRoute();
-        FXRouter.goTo("Homepage");
+        FXRouter.goTo("RootLayout");
     }
 
     private static void configRoute() {
         String packageStr = "ku/cs/usecasedesigner/";
 
-        FXRouter.when("UseCaseDesigner", packageStr + "CubicCurveDemo.fxml", "UseCaseDesigner | UseCaseDesigner");
+        FXRouter.when("CubicCurveDemo", packageStr + "CubicCurveDemo.fxml", "UseCaseDesigner | CubicCurveDemo");
         FXRouter.when("DraggableNode", packageStr + "DraggableNode.fxml", "UseCaseDesigner | DraggableNode");
         FXRouter.when("DragIcon", packageStr + "DragIcon.fxml", "UseCaseDesigner | DragIcon");
         FXRouter.when("NodeLink", packageStr + "NodeLink.fxml", "UseCaseDesigner | NodeLink");
