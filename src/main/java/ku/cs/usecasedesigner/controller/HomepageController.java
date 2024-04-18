c6035bf12264095caf73f71ccdcec97eb73228ba
Thanadon Mhp <thanadon.mhp@gmail.com>
2024-04-19 02:05:04 +0700

    Fix node selection.

--- src/main/java/ku/cs/usecasedesigner/controller/HomepageController.java
+++ src/main/java/ku/cs/usecasedesigner/controller/HomepageController.java
@@ -33,6 +33,8 @@ public class HomepageController {
     public void PaneDragDropped(DragEvent dragEvent) {
         if(dragEvent.getDragboard().hasString()) {
             ImageView imageView = new ImageView();
+
+            // Set the image of the component
             if(dragEvent.getDragboard().getString().equals("Oval")) {
                 imageView.setImage(ovalImageView.getImage());
                 System.out.println("Oval Dropped");
@@ -40,27 +42,20 @@ public class HomepageController {
                 imageView.setImage(actorImageView.getImage());
                 System.out.println("Actor Dropped");
             }
+
+            // Set the size and position of the component
             imageView.setFitWidth(150);
             imageView.setFitHeight(150);
             imageView.setLayoutX(dragEvent.getX() - 75);
             imageView.setLayoutY(dragEvent.getY() - 75);
+
+            // Add the component to the design pane
             designPane.getChildren().add(imageView);
 
+            // Make the component draggable
             MakeDraggable(designPane.getChildren().get(designPane.getChildren().size() - 1));
-
-            designPane.getChildren().get(designPane.getChildren().size() - 1).addEventHandler(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() {
-                @Override
-                public void handle(MouseEvent mouseEvent) {
-                    if (mouseEvent.getClickCount() == 1) {
-                        System.out.println("Item Clicked");
-                        designPane.getChildren().get(designPane.getChildren().size() - 1).setStyle("-fx-border-color: red");
-                    }
-                    if (mouseEvent.getClickCount() == 2) {
-                        designPane.getChildren().remove(designPane.getChildren().size() - 1);
-                        System.out.println("Item Removed");
-                    }
-                }
-            });
+            // Make the component selectable
+            MakeSelectable(designPane.getChildren().get(designPane.getChildren().size() - 1));
         }
     }
 
@@ -90,6 +85,22 @@ public class HomepageController {
         node.setOnMouseDragged(e -> {
             node.setTranslateX(e.getSceneX() - startX);
             node.setTranslateY(e.getSceneY() - startY);
+        });
+    }
+
+    private void MakeSelectable(Node node) {
+        node.addEventHandler(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                if (mouseEvent.getClickCount() == 1) {
+                    System.out.println("Item Clicked");
+                    node.setStyle("-fx-border-color: red; -fx-border-width: 2px;");
+                }
+                if (mouseEvent.getClickCount() == 2) {
+                    designPane.getChildren().remove(node);
+                    System.out.println("Item Removed");
+                }
+            }
         });
     }
 }
