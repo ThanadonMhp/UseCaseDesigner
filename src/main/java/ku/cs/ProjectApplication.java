f629d581cbf6f1e78de654ed08216a83b8d9d284
Thanadon Mhp <thanadon.mhp@gmail.com>
2024-05-05 01:17:34 +0700

    Remove unused import

--- src/main/java/ku/cs/ProjectApplication.java
+++ src/main/java/ku/cs/ProjectApplication.java
@@ -1,21 +1,9 @@
 package ku.cs;
 
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.DatePicker;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.VBox;
-import javafx.stage.FileChooser;
-import javafx.stage.Modality;
 import ku.cs.fxrouter.FXRouter;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import java.io.IOException;
-import java.time.LocalDate;
 
 public class ProjectApplication extends Application {
     @Override
