5ca2d2fdb46fd6ef36dbecf4ecd6c2e9cff8fcce
Thanadon Mhp <thanadon.mhp@gmail.com>
2024-05-05 21:51:37 +0700

    New Node linking for Use Case

--- src/main/java/ku/cs/usecasedesigner/models/ConnectionList.java
+++ src/main/java/ku/cs/usecasedesigner/models/ConnectionList.java
@@ -3,6 +3,7 @@ package ku.cs.usecasedesigner.models;
 import javafx.scene.Node;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 
 import java.util.ArrayList;
@@ -29,16 +30,24 @@ public class ConnectionList {
     public Node findNodeByPosition(double x, double y, Pane pane) {
         // search for an item in pane that is close to the given position (x, y) and return it as a Node
         for (Node node : pane.getChildren()) {
-            if (!(node instanceof VBox)) {
-                continue;
-            }
-            double nodeMinX = node.getLayoutX();
-            double nodeMaxX = node.getLayoutX() + ((ImageView) ((VBox) node).getChildren().get(0)).getFitWidth();
-            double nodeMinY = node.getLayoutY();
-            double nodeMaxY = node.getLayoutY() + ((ImageView) ((VBox) node).getChildren().get(0)).getFitHeight();
+            if (node instanceof VBox) {
+                double nodeMinX = node.getLayoutX();
+                double nodeMaxX = node.getLayoutX() + ((ImageView) ((VBox) node).getChildren().get(0)).getFitWidth();
+                double nodeMinY = node.getLayoutY();
+                double nodeMaxY = node.getLayoutY() + ((ImageView) ((VBox) node).getChildren().get(0)).getFitHeight();
 
-            if (x >= nodeMinX && x <= nodeMaxX && y >= nodeMinY && y <= nodeMaxY) {
-                return node;
+                if (x >= nodeMinX && x <= nodeMaxX && y >= nodeMinY && y <= nodeMaxY) {
+                    return node;
+                }
+            } else if (node instanceof StackPane) {
+                double nodeMinX = node.getLayoutX();
+                double nodeMaxX = node.getLayoutX() + ((ImageView) ((StackPane) node).getChildren().get(0)).getFitWidth();
+                double nodeMinY = node.getLayoutY();
+                double nodeMaxY = node.getLayoutY() + ((ImageView) ((StackPane) node).getChildren().get(0)).getFitHeight();
+
+                if (x >= nodeMinX && x <= nodeMaxX && y >= nodeMinY && y <= nodeMaxY) {
+                    return node;
+                }
             }
         }
         return null;
