190058de7165af9541d59517d1d4e6a37fd5d949
Pariphatt <pariphat.m@ku.th>
2024-05-06 19:44:43 +0700

    ui fix

--- src/main/java/ku/cs/ProjectApplication.java
+++ src/main/java/ku/cs/ProjectApplication.java
@@ -1,5 +1,9 @@
 package ku.cs;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.StageStyle;
 import ku.cs.fxrouter.FXRouter;
 import javafx.application.Application;
 import javafx.stage.Stage;
@@ -12,7 +16,23 @@ public class ProjectApplication extends Application {
         configRoute();
         FXRouter.goTo("HomePage");
         FXRouter.popup("LandingPage");
-    }
+
+
+//        // Create a new stage
+//        Stage popupStage = new Stage();
+//        popupStage.initStyle(StageStyle.UNDECORATED); // Remove window decorations
+//
+//// Load the FXML file
+//        FXMLLoader loader = new FXMLLoader(getClass().getResource("src/main/resources/ku/cs/usecasedesigner/landing-page.fxml"));
+//        System.out.println(loader);
+//        Parent root = loader.load();
+//
+//// Set the scene and show the popup
+//        Scene scene = new Scene(root);
+//        popupStage.setScene(scene);
+//        popupStage.setResizable(false); // Disable resizing
+//        popupStage.show();
+   }
 
     private static void configRoute() {
         String packageStr = "ku/cs/usecasedesigner/";
