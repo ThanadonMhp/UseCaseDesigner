f59fc8719575e021dd1c36e01b77e2b2016c753a
Thanadon Mhp <thanadon.mhp@gmail.com>
2024-05-17 21:52:09 +0700

    New function for UseCase description edit.

--- src/main/java/ku/cs/usecasedesigner/controller/UseCasePageController.java
+++ src/main/java/ku/cs/usecasedesigner/controller/UseCasePageController.java
@@ -56,6 +56,10 @@ public class UseCasePageController {
             {
                 preConditionTextField.setText(useCase.getPreCondition());
             }
+            if (useCase.getPostCondition() != "!@#$%^&*()_+")
+            {
+                postConditionTextField.setText(useCase.getPostCondition());
+            }
         }
     }
 
