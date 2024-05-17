938cb49a5f675c4adca65c87128aa289c3aa204a
Thanadon Mhp <thanadon.mhp@gmail.com>
2024-05-17 21:00:46 +0700

    New function for Preference data writing

--- src/main/java/ku/cs/usecasedesigner/controller/PreferencePageController.java
+++ src/main/java/ku/cs/usecasedesigner/controller/PreferencePageController.java
@@ -1,4 +1,40 @@
 package ku.cs.usecasedesigner.controller;
 
+import javafx.fxml.FXML;
+import ku.cs.fxrouter.FXRouter;
+import ku.cs.usecasedesigner.models.Preference;
+import ku.cs.usecasedesigner.models.PreferenceList;
+import ku.cs.usecasedesigner.services.DataSource;
+import ku.cs.usecasedesigner.services.PreferenceListFileDataSource;
+
+import java.util.ArrayList;
+
 public class PreferencePageController {
+
+    private String directory;
+    private String projectName;
+
+    // This method is called when the FXML file is loaded
+    // Load the data from the previous page
+    @FXML void initialize() {
+        if (FXRouter.getData() != null) {
+            ArrayList<Object> objects = (ArrayList) FXRouter.getData();
+            directory = (String) objects.get(0);
+            projectName = (String) objects.get(1);
+        }
+
+    }
+
+    // Save the preference to the file
+    public void savePreference(int strokeWidth, String font, int fontSize, String theme) {
+        // Save the preference to the file
+        PreferenceList preferenceList = new PreferenceList();
+        DataSource<PreferenceList> preferenceListDataSource = new PreferenceListFileDataSource(directory, projectName);
+        Preference preference = new Preference(strokeWidth, font, fontSize, theme);
+        preferenceList.addPreference(preference);
+
+        preferenceListDataSource.writeData(preferenceList);
+    }
+
+
 }
