fb96e732da4d2e13ee180ba0ea8f89faa1534744
Thanadon Mhp <thanadon.mhp@gmail.com>
2024-05-17 22:00:14 +0700

    Code cleaning

--- src/main/java/ku/cs/usecasedesigner/controller/LabelPageController.java
+++ src/main/java/ku/cs/usecasedesigner/controller/LabelPageController.java
@@ -3,7 +3,6 @@ package ku.cs.usecasedesigner.controller;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
-import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
